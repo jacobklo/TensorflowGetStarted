d45d40c06f7a96b4d54fa23f5010b5c931493e19
Jacob Lo <it@jacoblo.net>
2019-06-25 20:59:14 -0700

    continue fashion MNIST

--- fashion_MNIST_basics.py
+++ fashion_MNIST_basics.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
+import random
 from matplotlib import pyplot
-
+from matplotlib import animation
 
 class FashionMNIST:
 	def __init__(self):
@@ -10,6 +11,7 @@ class FashionMNIST:
 		self.test_images = None
 		self.test_labels = None
 		self.class_names = None
+		self.model = None
 
 		self.load()
 		self.prepare()
@@ -26,11 +28,54 @@ class FashionMNIST:
 		self.train_images = self.train_images / 255
 		self.test_images = self.test_images / 255
 
+	def build_model(self):
+		self.model = tf.keras.Sequential([
+			tf.keras.layers.Flatten(input_shape=(28, 28)),
+			tf.keras.layers.Dense(10, activation='softmax')
+		])
+
+		self.model.compile(optimizer='adam',
+					  loss='sparse_categorical_crossentropy',
+					  metrics=['accuracy'])
+
+	def train_model(self, epochs=10):
+		self.model.fit(self.train_images, self.train_labels, epochs)
+
+	def evaluate_model(self):
+		test_loss, test_acc = self.model.evaluate(self.test_images, self.test_labels)
+		print('\nTest accuracy:', test_acc)
+
+	# TODO: Predict
+	
 	def plot_one(self, i: int):
 		fig = pyplot.figure()
-		fig.imshow(self.train_images[i])
-		fig.colorbar()
-		fig.grid(False)
-		fig.show()
-		# TODO : dynmic update specific plot
+		pyplot.imshow(self.train_images[i])
+		pyplot.colorbar()
+		pyplot.grid(False)
+		pyplot.show()
 
+	def plot_one_random(self):
+		fig = pyplot.figure()
+		data = self.train_images
+		im = pyplot.imshow(data[0])
+		nx, ny = 28, 28
+
+		def animate(_):
+			ran_pic_idx = random.randint(-1, len(data) - 1)
+			im.set_data(data[ran_pic_idx])
+			return im, 10
+
+		anim = animation.FuncAnimation(fig, animate, frames=nx * ny, interval=1000)
+		pyplot.show()
+
+	def plot_multiple(self):
+		fig = pyplot.figure(figsize=(10, 10))
+
+		for i in range(25):
+			pyplot.subplot(5,5, i+1)
+			pyplot.xticks([])
+			pyplot.yticks([])
+			pyplot.grid(False)
+			pyplot.imshow(self.train_images[i])
+			pyplot.xlabel(self.class_names[self.train_labels[i]])
+		pyplot.show()
